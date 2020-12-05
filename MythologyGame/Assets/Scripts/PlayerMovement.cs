082b2efe46280f9bd7a4461d57709ba3c8ed41dc
DESKTOP-AM6HGNM\Yona Harel <yona.harel6@gmail.com>
2020-12-05 19:17:50 +0200

    first commit

---
+++ MythologyGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+    private bool facingRight = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+       animator.SetFloat("Horizontal", movement.y);
+       animator.SetFloat("Vertical", movement.x);
+       animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        Vector3 localScale = transform.localScale;
+
+        if (movement.x < 0 && facingRight)
+        {
+            facingRight = false;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+        
+        if (!facingRight && movement.x > 0)
+        {
+            facingRight = true;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
